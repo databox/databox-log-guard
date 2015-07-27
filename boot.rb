16e871bf05ee2815a1cca476d02403883faa5bfd
Oto Brglez <otobrglez@gmail.com>
2015-07-28 00:34:00 +0200

    Update

--- boot.rb
+++ boot.rb
@@ -9,6 +9,6 @@ require 'yaml'
 require 'erb'
 require 'haml'
 
-ENV['LOG_GUARD_ENV'] ||= 'development'
+ENV['LOG_GUARD_ENV'] ||= ENV['LOG_GUARD_ENV'] || ENV['RACK_ENV'] || 'development'
 
 CONFIG = YAML.load(ERB.new(IO.read("./config/config.yml")).result)[ENV['LOG_GUARD_ENV']]
