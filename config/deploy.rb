79ce1d440f051ee9b57ff63ae7e9050f5bf6c365
Oto Brglez <otobrglez@gmail.com>
2015-07-27 16:22:06 +0200

    Update

--- config/deploy.rb
+++ config/deploy.rb
@@ -53,7 +53,7 @@ namespace :deploy do
   desc "Start the Unicorn process when it isn't already running."
   task :start do
     on roles(:all) do
-      execute "bundle exec unicorn -c #{shared_path}/config/unicorn.rb -p 8080 -E production"
+      execute "unicorn -c #{shared_path}/config/unicorn.rb -p 8080 -E production"
       # execute "bundle exec unicorn -D -c #{shared_path}/config/unicorn.rb -E production -p 8080"
     end
   end
