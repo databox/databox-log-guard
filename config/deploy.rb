6327b3ec1acf2e836eac0d0d3a4b4855b3638bed
Oto Brglez <otobrglez@gmail.com>
2015-07-27 15:49:25 +0200

    Adding stuff..

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ lock '3.4.0'
 
 set :application, 'log-guard'
 # set :repo_url, 'git@example.com:me/my_repo.git'
+set :repo_url, 'git@github.com:databox/databox-log-guard.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
