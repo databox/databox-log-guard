c36554a905cfc35ad9bbf62e1f7a76d993509920
Oto Brglez <otobrglez@gmail.com>
2015-07-27 16:10:46 +0200

    Update

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,10 +28,10 @@ set :deploy_via, :copy
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/config.yml')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp', 'vendor/bundle')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -49,5 +49,7 @@ namespace :deploy do
       # end
     end
   end
+
+
 
 end
